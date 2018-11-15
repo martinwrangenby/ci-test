cd1f04e09cb4b4c9540bf51bad581f9c3ec7de61
Martin Wrangenby <martinwrangenby@gmail.com>
2018-11-15 12:08:15 +0100

    new test

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,8 @@ const sum = require('./index');
 test('summation works correctly', () => {
   expect(sum(1,2)).toBe(3);
 });
+
+
+test('summations works for negative integers', () => {
+  expect(sum(-3, -4)).toBe(-7);
+});
