9208166ca7682e17304680da5ce9118484607de2
Martin Wrangenby <martinwrangenby@gmail.com>
2018-11-15 21:32:44 +0100

    added a few more tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-function sum(a,b) {
-  return a + b;
+function sum(a = 0, b = 0) {
+  let result;
+  if (typeof a != 'number' || typeof b != 'number') {
+    result = NaN;
+  } else {
+    result = a + b
+  }
+  return result;
 };
 
 module.exports = sum;
--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,14 @@ test('summation works correctly', () => {
   expect(sum(1,2)).toBe(3);
 });
 
-
 test('summations works for negative integers', () => {
   expect(sum(-3, -4)).toBe(-7);
 });
+
+test('Incorrect input parameter should throw an error', () => {
+  expect(sum('2', '4')).toBe(NaN);
+});
+
+test('missing input parameters defaults to 0', () => {
+  expect(sum()).toBe(0);
+})
