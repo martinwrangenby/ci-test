c2fb660cd2d1389bbff3e446e674082af06cd693
Martin Wrangenby <martinwrangenby@gmail.com>
2018-11-15 21:41:09 +0100

    wrapped tests in suite

--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,19 @@
 const sum = require('./index');
 
-test('summation works correctly', () => {
-  expect(sum(1,2)).toBe(3);
+describe('verify sum()', () => {
+  test('summation works correctly', () => {
+    expect(sum(1,2)).toBe(3);
+  });
+  
+  test('summation works for negative integers', () => {
+    expect(sum(-3, -4)).toBe(-7);
+  });
+  
+  test('incorrect input parameter should return NaN', () => {
+    expect(sum('2', '4')).toBe(NaN);
+  });
+  
+  test('missing input parameters defaults to 0', () => {
+    expect(sum()).toBe(0);
+  });
 });
-
-test('summations works for negative integers', () => {
-  expect(sum(-3, -4)).toBe(-7);
-});
-
-test('Incorrect input parameter should throw an error', () => {
-  expect(sum('2', '4')).toBe(NaN);
-});
-
-test('missing input parameters defaults to 0', () => {
-  expect(sum()).toBe(0);
-})
